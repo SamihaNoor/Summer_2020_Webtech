9bca4e323776f0de624b81a6227890d4f353bb0b
Your Name <you@example.com>
2020-08-04 09:01:16 +0600

    reg

---
+++ Mini-Project/regCheck.php
@@ -0,0 +1,31 @@
+<?php
+	session_start();
+
+	if(isset($_POST['submit'])){
+
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$confirmpassword = $_POST['confirmpassword'];
+		$id = $_POST['id'];
+		$type = $_POST['type'];
+
+		if(empty($name) || empty($password) || empty($email) || empty($id) || empty($confirmpassword) || empty($type)){
+			echo "null submission";
+		}else {
+			/*$_SESSION['uname'] 		= $uname;
+			$_SESSION['email'] 		= $email;
+			$_SESSION['password'] 	= $password;
+			$_SESSION['user'] 		= $user;*/
+
+
+			$file = fopen('user.txt', 'w');
+			fwrite($file, $id.'|'.$name.'|'.$password.'|'.$email.'|'.$type,"/r/n");
+			fclose($file);
+
+			header('location: login.html');
+		}
+	}else{
+		header("location: login.html");
+	}
+?>
