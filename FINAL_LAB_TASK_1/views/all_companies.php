db1c038c1709b1994b22deb690a5bff79d977bde
Your Name <you@example.com>
2020-08-18 22:24:05 +0600

    list all companies

---
+++ FINAL_LAB_TASK_1/views/all_companies.php
@@ -0,0 +1,53 @@
+<?php
+	require_once('../php/session_header.php');
+	require_once('../service/userService.php');
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User List</title>
+</head>
+<body>
+
+	<a href="home.php">Back</a> |
+	<a href="../php/logout.php">Logout</a> 
+	
+	<h3>Company list</h3>
+
+	<table border="1">
+		<tr>
+			<td>ID</td>
+			<td>Company Name</td>
+			<td>Description</td>
+			<td>Industry</td>
+			<td>Website</td>
+			<td>Logo</td>
+			<td>Added By</td>
+			<td>Action</td>
+		</tr>
+
+		<?php  
+			$companies = getAllCompanies();
+			for ($i=0; $i != count($companies); $i++) 
+			{  ?>
+			<tr>
+				<td><?=$companies[$i]['id']?></td>
+				<td><?=$companies[$i]['company_name']?></td>
+				<td><?=$companies[$i]['profile_description']?></td>
+				<td><?=$companies[$i]['industry']?></td>
+				<td><?=$companies[$i]['company_website']?></td>
+				<td><img src="<?=$companies[$i]['company_logo']?>" width="100" height="100"></td>
+				<td><?=$companies[$i]['user_account_id']?></td>
+				<td>
+					<a href="edit.php?id=<?=$companies[$i]['id']?>">Edit</a> |
+					<a href="delete.php?id=<?=$companies[$i]['id']?>">Delete</a> 
+				</td>
+			</tr>
+
+		<?php } ?>
+		
+	</table>
+</body>
+</html>
--- FINAL_LAB_TASK_1/views/home.php
+++ FINAL_LAB_TASK_1/views/home.php
@@ -12,6 +12,7 @@
 	<a href="../views/create.php">Create New User</a> |
 	<a href="../views/all_users.php">User List</a> |
 	<a href="../php/logout.php">Logout</a> <br>
-	<a href="../views/addCompany.php?">Add New Company</a>
+	<a href="../views/addCompany.php?">Add New Company</a> |
+	<a href="../views/all_companies.php">Company List</a>
 </body>
 </html>
