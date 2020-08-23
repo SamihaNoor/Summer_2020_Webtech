9ef238355b3ddbf9e53d7b474ef4201a1fca850d
Your Name <you@example.com>
2020-08-23 22:59:33 +0600

    date of birth

---
+++ FINAL_LAB_TASK_2/4.date/script.js
@@ -0,0 +1,30 @@
+function validate()
+{
+	var d = document.forms["form"]["dd"].value;
+	var m = document.forms["form"]["mm"].value;
+	var y = document.forms["form"]["yyyy"].value;
+	if (d == "" || m=="" || y=="") 
+	{
+		alert("Date cannot be empty");
+		return false;
+	}
+	else if(d<1 || d>31)
+	{
+		alert("Please day between 1-31");
+		return false;
+	}
+	else if(m<1 || m>12)
+	{
+		alert("Please day between 1-12");
+		return false;
+	}
+	else if(y<1900 || y>2016)
+	{
+		alert("Please day between 1900-2016");
+		return false;
+	}
+	else
+	{ 
+		return true;
+	}
+}
