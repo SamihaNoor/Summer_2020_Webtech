f6565b57af00789b2e5794dddd0ddf1d50fa3317
Your Name <you@example.com>
2020-08-23 22:11:18 +0600

    name

---
+++ FINAL_LAB_TASK_2/1.name/script.js
@@ -0,0 +1,23 @@
+function validate()
+{
+	var n = document.forms["form"]["name"].value;
+	if (n == "") 
+	{
+		alert("Please enter name first");
+		return false;
+	}
+	else if((n.charAt(0) <= 'a' || n.charAt(0) >= 'z') && (n.charAt(0) <= 'A' || n.charAt(0) >= 'Z'))
+	{
+		alert("Name must start with a letter");
+		return false;
+	}
+	else if(n.split(" ").length<2)
+	{
+		alert("Name must be at least two words");
+		return false;
+	}
+	else
+	{ 
+		return true;
+	}
+}
