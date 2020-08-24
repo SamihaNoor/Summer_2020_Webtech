d42918f4b02e69043c3ea2d5ec2370b668d01171
Your Name <you@example.com>
2020-08-24 23:04:07 +0600

    email innerHTML

--- FINAL_LAB_TASK_2/2.email/script.js
+++ FINAL_LAB_TASK_2/2.email/script.js
@@ -3,12 +3,14 @@ function validate()
 	var n = document.forms["form"]["email"].value;
 	if (n == "") 
 	{
-		alert("Please enter email first");
+		//alert("Please enter email first");
+		document.getElementById("error").innerHTML="Please enter email first";
 		return false;
 	}
 	if (n.indexOf("@")<0 || n.indexOf(".com")<0) 
 	{
-		alert("Please a valid email");
+		//alert("Please a valid email");
+		document.getElementById("error").innerHTML="Please enter valid email first";
 		return false;
 	}
 	else
