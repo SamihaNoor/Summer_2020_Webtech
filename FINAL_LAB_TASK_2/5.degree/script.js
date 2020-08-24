f25d8d2c1647b730aaaea70a4002ae4aa5f4a898
Your Name <you@example.com>
2020-08-24 19:56:49 +0600

    degree

--- FINAL_LAB_TASK_2/5.degree/script.js
+++ FINAL_LAB_TASK_2/5.degree/script.js
@@ -1,14 +1,27 @@
 function validate()
 {
-	var d = document.forms["form"]["degree"].value;
+	var d = document.forms["form"]["degree[]"];
+	var check=false;
 	
-	if(d == "") 
+	if(d[0].checked) 
 	{
-		alert("You must select one");
-		return false;
+		check=true;
+	}
+	else if(d[1].checked) 
+	{
+		check=true;
+	}
+	else if(d[2].checked) 
+	{
+		check=true;
+	}
+	if(check)
+	{
+		return true;
 	}
 	else
 	{ 
-		return true;
+		alert("You must select one");
+		return false;
 	}
 }
