690d7fe1423be45f91f91d314b39e2bc9f1fc4d6
Your Name <you@example.com>
2020-08-24 23:10:29 +0600

    date innerHTML

--- FINAL_LAB_TASK_2/4.date/script.js
+++ FINAL_LAB_TASK_2/4.date/script.js
@@ -5,22 +5,26 @@ function validate()
 	var y = document.forms["form"]["yyyy"].value;
 	if (d == "" || m=="" || y=="") 
 	{
-		alert("Date cannot be empty");
+		//alert("Date cannot be empty");
+		document.getElementById("error").innerHTML="Date cannot be empty";
 		return false;
 	}
 	else if(d<1 || d>31)
 	{
-		alert("Please day between 1-31");
+		//alert("Please day between 1-31");
+		document.getElementById("error").innerHTML="Please day between 1-31";
 		return false;
 	}
 	else if(m<1 || m>12)
 	{
-		alert("Please day between 1-12");
+		//alert("Please day between 1-12");
+		document.getElementById("error").innerHTML="Please day between 1-12";
 		return false;
 	}
 	else if(y<1900 || y>2016)
 	{
-		alert("Please day between 1900-2016");
+		//alert("Please day between 1900-2016");
+		document.getElementById("error").innerHTML="Please day between 1900-2016";
 		return false;
 	}
 	else
